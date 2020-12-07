69b15c75514ac4e94f04019c0d26b5fb2e7f3c25
LeonSoares <leonsoares3@gmail.com>
2020-12-07 16:32:13 -0500

    add all movies Row to UI with basic style

--- src/requests.js
+++ src/requests.js
@@ -3,12 +3,12 @@ const apiKey = "b0eda2a2066bc2e5bb922102e2025b91"
 const request = {
     fetchTrending: `/trending/all/week?api_key=${apiKey}&language=`,
     fetchNetFlixOriginals: `discover/tv?api_key=${apiKey}&with_networks=213`,
-    fetchTopRated: `/movie/top_rated?api?api_key=${apiKey}&language=en-US`,
-    fetchActionMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=28`,
-    fetchComedyMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=35`,
-    fetchHorrorMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=27`,
-    fetchRomanceMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=10749`,
-    fetchDocumentaries: `/movie/top_rated?api?api_key=${apiKey}&with_genres=99`
+    fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${apiKey}&with_genres=99`
 }
 
 
