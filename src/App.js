7c72ab0b0667e1d7d593fb425afd8f466a19bf1e
LeonSoares <leonsoares3@gmail.com>
2020-12-07 14:50:47 -0500

    set up API requests patterns with axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 // import logo from './logo.svg';
 import './App.css';
+import Row from './Row'
+import requests from './requests'
+
+
+
 
 function App() {
   return (
     <div className="App">
       <h1>8kprime video</h1>
+      < Row title="NETFLIX ORIGINALS"  fetchUrl={requests.fetchNetFlixOriginals}/>
+      < Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      
     </div>
   );
 }
--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,15 @@
-const apiKey = process.env.TMDB_KEY
+const apiKey = "b0eda2a2066bc2e5bb922102e2025b91"
 
 const request = {
-    
+    fetchTrending: `/trending/all/week?api_key=${apiKey}&language=`,
+    fetchNetFlixOriginals: `discover/tv?api_key=${apiKey}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api?api_key=${apiKey}&language=en-US`,
+    fetchActionMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=28`,
+    fetchComedyMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=35`,
+    fetchHorrorMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=27`,
+    fetchRomanceMovies: `/movie/top_rated?api?api_key=${apiKey}&with_genres=10749`,
+    fetchDocumentaries: `/movie/top_rated?api?api_key=${apiKey}&with_genres=99`
 }
 
 
-export default requests
+export default request
