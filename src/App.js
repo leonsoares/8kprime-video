cf9d1b8451e492c4fc5a7efb65d9ff6436af2abd
LeonSoares <leonsoares3@gmail.com>
2020-12-09 12:36:51 -0500

    change banners size and layout

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+
       <Banner />
       < Row 
         title="NETFLIX ORIGINALS"  
@@ -18,13 +19,14 @@ function App() {
       < Row 
       title="Trending Now" 
       fetchUrl={requests.fetchTrending}
+      isLargeRow
       />
-      < Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
-      < Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
-      < Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
-      < Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
-      < Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
-      < Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+      < Row title="Top Rated" fetchUrl={requests.fetchTopRated} isLargeRow/>
+      < Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow/>
+      < Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow/>
+      < Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow/>
+      < Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow/>
+      < Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow/>
     </div>
   );
 }
